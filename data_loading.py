847306fcb5ce8093f385cc0cab79fabf2c47b168
AliM100 <axelaxel30000@gmail.com>
2022-11-23 17:22:56 +0200

    first commit

---
+++ data_loading.py
@@ -0,0 +1,88 @@
+from PIL import Image
+import sys
+import cv2
+import numpy as np 
+import os
+from matplotlib import pyplot as plt
+import shutil
+from tqdm import tqdm
+from pathlib import Path
+import random
+from keras.utils.np_utils import to_categorical
+from keras.preprocessing.image import ImageDataGenerator
+
+def scan_folder(parent,target):
+    # iterate over all the files in directory 'parent'
+    current_path=parent
+    for file_name in os.listdir(parent):
+        if file_name.endswith(".png"):
+            shutil.move(os.path.join(current_path,file_name),target)
+        else:
+            current_path = "".join((parent, "/", file_name))
+            if os.path.isdir(current_path):
+                # if we're checking a sub-directory, recursively call this method
+                scan_folder(current_path,target)
+
+def distribute_data():
+    path="BreaKHis_v1/histology_slides/breast"
+    ben="benign/SOB"
+    mal="malignant/SOB"
+    data="data"
+    os.mkdir(data)
+    benign="data/benign"
+    malignant="data/malignant"
+    os.mkdir(benign)
+    scan_folder(os.path.join(path,ben),benign)
+    os.mkdir(malignant)
+    scan_folder(os.path.join(path,mal),malignant)
+
+RNG = np.random.RandomState(4321)
+
+def split_train_test(args):
+    os.mkdir(args.dataset_dir+"/"+args.train_sub_dir)
+    os.mkdir(args.dataset_dir+"/"+args.valid_sub_dir)
+    os.mkdir(args.dataset_dir+"/"+args.test_sub_dir)
+    
+    for i in random.sample(os.listdir(args.benign_dir),int(len(os.listdir(args.benign_dir))*.7)):
+        shutil.move(os.path.join(args.benign_dir,i),os.path.join(args.dataset_dir,args.train_sub_dir))
+    for i in random.sample(os.listdir(args.malig_dir),int(len(os.listdir(args.malig_dir))*.7)):
+        shutil.move(os.path.join(args.malig_dir,i),os.path.join(args.dataset_dir,args.train_sub_dir))
+    for i in random.sample(os.listdir(args.benign_dir),int(len(os.listdir(args.benign_dir))*(1/3))):
+        shutil.move(os.path.join(args.benign_dir,i),os.path.join(args.dataset_dir,args.valid_sub_dir))    
+    for i in random.sample(os.listdir(args.malig_dir),int(len(os.listdir(args.malig_dir))*(1/3))):
+        shutil.move(os.path.join(args.malig_dir,i),os.path.join(args.dataset_dir,args.valid_sub_dir))
+    for i in random.sample(os.listdir(args.benign_dir),int(len(os.listdir(args.benign_dir)))):
+        shutil.move(os.path.join(args.benign_dir,i),os.path.join(args.dataset_dir,args.test_sub_dir))    
+    for i in random.sample(os.listdir(args.malig_dir),int(len(os.listdir(args.malig_dir)))):
+        shutil.move(os.path.join(args.malig_dir,i),os.path.join(args.dataset_dir,args.test_sub_dir))    
+
+
+    
+        
+def load(sub_dir, args, rng=RNG, resize=None,test=False):
+        
+        IMG = []
+        Y=[]
+        images=[]
+        read = lambda imname: np.asarray(Image.open(imname).convert("RGB"))
+        for IMAGE_NAME in tqdm(os.listdir(args.dataset_dir+"/"+sub_dir)):
+                PATH = os.path.join(args.dataset_dir+"/"+sub_dir,IMAGE_NAME)
+                _, ftype = os.path.splitext(PATH)
+                if ftype == ".png":
+                    img = read(PATH)
+                    img = cv2.resize(img, (resize,resize))
+                    IMG.append(np.array(img))
+                    e=IMAGE_NAME.split("_")[1]
+                    if e=="B":
+                        Y.append(0)
+                    elif e=="M":
+                        Y.append(1)
+                    images.append(IMAGE_NAME) 
+        Y = to_categorical(Y, num_classes= 2)
+        if test==True:
+            return np.array(IMG),np.array(Y),images
+        else:
+            return np.array(IMG),np.array(Y)   
+
+     
+
---
+++ main.py
@@ -0,0 +1,119 @@
+from PIL import Image
+import sys
+import cv2
+import numpy as np 
+from matplotlib import pyplot as plt
+import data_loading
+import argparse
+import os
+from keras.preprocessing.image import ImageDataGenerator
+import model_
+from tqdm import tqdm
+from keras.applications import ResNet50
+from keras.callbacks import Callback, ModelCheckpoint, ReduceLROnPlateau, TensorBoard
+from sklearn.metrics import cohen_kappa_score, accuracy_score
+import tensorflow as tf
+import gc
+import pandas as pd
+import pickle
+import json
+
+
+def train(args):
+    X_train,Y_train=data_loading.load(sub_dir=args.train_sub_dir,args=args,resize=224)
+    x_val,y_val=data_loading.load(sub_dir=args.valid_sub_dir,args=args,resize=224)
+    train_generator = ImageDataGenerator(
+        zoom_range=2,  # set range for random zoom
+        rotation_range = 90,
+        horizontal_flip=True,  # randomly flip images
+        vertical_flip=True,  # randomly flip images
+    )
+    gc.collect()
+    resnet = ResNet50(
+    weights='imagenet',
+    include_top=False,
+    input_shape=(224,224,3)
+)
+    Model=model_.build_model(resnet,lr=args.learning_rate)
+    # Learning Rate Reducer
+    learn_control = ReduceLROnPlateau(monitor='val_accuracy', patience=5,
+                                  verbose=1,factor=0.2, min_lr=1e-7)
+    #save best checkpoint    
+    check="weights"
+    os.mkdir(check)                          
+    checkpoint_file=args.checkpoint_dir
+    checkpoint = tf.keras.callbacks.ModelCheckpoint(checkpoint_file, monitor='val_accuracy', verbose=1, save_best_only=True, mode='max')
+   
+    history = Model.fit(
+    train_generator.flow(X_train, Y_train, batch_size=args.batch_size),
+    steps_per_epoch=X_train.shape[0] / args.batch_size,
+    epochs=args.num_epochs,
+    validation_data=(x_val, y_val),
+    callbacks=[learn_control, checkpoint]
+)
+    #save history in to json file
+    
+    with open(args.history_dir,mode='w') as f:
+        json.dump(str(history.history),f)
+        hdf = pd.DataFrame(history.history)
+        hdf[['accuracy', 'val_accuracy']].plot()
+        plt.show()
+        plt.savefig('acc.png')
+        hdf1= pd.DataFrame(history.history)
+        hdf1[['loss', 'val_loss']].plot()
+        plt.show()
+        plt.savefig('loss.png')
+
+def test(args):
+    resnet = ResNet50(
+    weights='imagenet',
+    include_top=False,
+    input_shape=(224,224,3)
+)
+    Model=model_.build_model(resnet,lr=args.learning_rate)
+    Model.load_weights(args.checkpoint_dir)
+
+   
+    x_test,y_test,images=data_loading.load(sub_dir=args.test_sub_dir,args=args,resize=224,test=True)
+    y_pred=Model.predict(x_test,batch_size=args.batch_size,
+                                        steps = len(x_test)/args.batch_size)
+
+    for i in range(len(images)):
+        print(f"Image: {images[i]}, original value: {np.argmax(y_test[i])}, predicted result: {np.argmax(y_pred[i])}")
+        
+    print(f"accuracy: {accuracy_score(np.argmax(y_test, axis=1), np.argmax(y_pred, axis=1))}")
+
+        
+
+
+
+
+if __name__ == "__main__":
+
+    args = argparse.Namespace(
+        checkpoint_dir="weights/best_weight.hdf5",
+        history_dir="weights/history.json",
+        dataset_dir="data",
+        benign_dir="data/benign",
+        malig_dir="data/malignant",
+        train_sub_dir="train",
+        valid_sub_dir="valid",
+        test_sub_dir="test",
+        gpus="0",
+        batch_size=16,
+        num_epochs=50,
+        train=True,
+        test=False,
+        learning_rate=1e-4,
+       
+    )
+    
+    os.environ["CUDA_VISIBLE_DEVICES"] = args.gpus
+    #data_loading.distribute_data()
+    #data_loading.split_train_test(args)
+    print(tf.config.list_physical_devices('GPU'))
+    with tf.device(tf.DeviceSpec(device_type="GPU", device_index=args.gpus)):
+        #train(args)
+        test(args)
+    
+
